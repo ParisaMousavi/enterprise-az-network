32c448c916258eb9f10a8c4d9de0314c80f717f4
Parisa Moosavinezhad <parisamoosavinezhad@yahoo.com>
2021-09-24 18:30:07 +0200

    add resource group for enterprise network

---
+++ main.tf
@@ -0,0 +1,20 @@
+module "resourcegroup" {
+  source = "git::https://bambgiqvuckxxvwjwvbjiasmxzbxt3oiucubtx534nky7at4qn7a@dev.azure.com/p-moosavinezhad/az-iac/_git/az-resourcegroup?ref=main"
+            // https://{PAT}@dev.azure.com/{organization}/{project}/_git/{repo-name}
+
+  region              = var.region
+ tags = {
+    Service           = "servivename"
+    AssetName         = "Asset Name"
+    AssetID           = "AB00CD"
+    BusinessUnit      = "Cloud Team"
+    Confidentiality   = "C1"
+    Integrity         = "I1"
+    Availability      = "A1"
+    Criticality       = "Low"
+    Owner             = "parisamoosavinezhad@hotmail.com"
+    CostCenter        = ""
+  }
+  resource_short_name = var.resource_short_name
+  resource_long_name  = var.resource_long_name
+}
