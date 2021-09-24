51c8071b77a46b347abb8354343321f17fb790c1
Parisa Moosavinezhad <parisamoosavinezhad@yahoo.com>
2021-09-24 19:39:27 +0200

    update variable values in first level module

--- main.tf
+++ main.tf
@@ -1,20 +1,21 @@
 module "resourcegroup" {
   source = "git::https://bambgiqvuckxxvwjwvbjiasmxzbxt3oiucubtx534nky7at4qn7a@dev.azure.com/p-moosavinezhad/az-iac/_git/az-resourcegroup?ref=main"
-           // "git::https://bambgiqvuckxxvwjwvbjiasmxzbxt3oiucubtx534nky7at4qn7a@dev.azure.com/p-moosavinezhad/az-iac/_git/az-resourcegroup?ref=main"
-           // https://{PAT}@dev.azure.com/{organization}/{project}/_git/{repo-name}
+  // "git::https://bambgiqvuckxxvwjwvbjiasmxzbxt3oiucubtx534nky7at4qn7a@dev.azure.com/p-moosavinezhad/az-iac/_git/az-resourcegroup?ref=main"
+  // https://{PAT}@dev.azure.com/{organization}/{project}/_git/{repo-name}
 
-  region              = var.region
+  region             = "westeurope"
+  resource_long_name = "dummy-company-name"
   tags = {
-    Service           = "servivename"
-    AssetName         = "Asset Name"
-    AssetID           = "AB00CD"
-    BusinessUnit      = "Cloud Team"
-    Confidentiality   = "C1"
-    Integrity         = "I1"
-    Availability      = "A1"
-    Criticality       = "Low"
-    Owner             = "parisamoosavinezhad@hotmail.com"
-    CostCenter        = ""
+    Service         = "servivename"
+    AssetName       = "Asset Name"
+    AssetID         = "AB00CD"
+    BusinessUnit    = "Cloud Team"
+    Confidentiality = "C1"
+    Integrity       = "I1"
+    Availability    = "A1"
+    Criticality     = "Low"
+    Owner           = "parisamoosavinezhad@hotmail.com"
+    CostCenter      = ""
   }
-  resource_long_name  = var.resource_long_name
+
 }
