468c59b7b6c154c87c1515efe342abff95731264
pmoosavi <parisa.moosavinezhad@computacenter.com>
2022-02-20 14:17:13 +0100

    add pip to module for bastion

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 locals {
-  resource_long_name = "${var.product}-${var.subscription}-${var.region_short}-${var.environment}"
+  repetation = ["cloudexcellence"]
 }
 
 module "resourcegroup" {
+  for_each = toset(local.repetation)
   source = "git::https://eh4amjsb2v7ke7yzqzkviryninjny3urbbq3pbkor25hhdbo5kea@dev.azure.com/p-moosavinezhad/az-iac/_git/az-resourcegroup?ref=main"
-  // "git::https://bambgiqvuckxxvwjwvbjiasmxzbxt3oiucubtx534nky7at4qn7a@dev.azure.com/p-moosavinezhad/az-iac/_git/az-resourcegroup?ref=main"
   // https://{PAT}@dev.azure.com/{organization}/{project}/_git/{repo-name}
 
   region             = "westeurope"
-  resource_long_name = "cloud-excellence-network-team"
+  resource_long_name = "network-team-${each.value}"
   tags = {
     Service         = "network"
     AssetName       = "Asset Name"
@@ -28,16 +28,35 @@ module "resourcegroup" {
 #       Enterprise Network
 #----------------------------------------------
 module "network" {
+  for_each = toset(local.repetation)
   source = "git::https://eh4amjsb2v7ke7yzqzkviryninjny3urbbq3pbkor25hhdbo5kea@dev.azure.com/p-moosavinezhad/az-iac/_git/az-vnet?ref=main"
 
   subscription            = var.subscription
   region_short            = var.region_short
   environment             = var.environment
-  product                 = var.product
-  resource_long_name      = local.resource_long_name
-  resource_group_name     = module.resourcegroup.name
-  resource_group_location = module.resourcegroup.location
+  product                 = "${each.value}"
+  resource_long_name      = "${each.value}-${var.subscription}-${var.region_short}-${var.environment}"
+  resource_group_name     = module.resourcegroup[each.value].name
+  resource_group_location = module.resourcegroup[each.value].location
 }
+
+
+module "pip" {
+  for_each = toset(local.repetation)
+  source = "git::https://eh4amjsb2v7ke7yzqzkviryninjny3urbbq3pbkor25hhdbo5kea@dev.azure.com/p-moosavinezhad/az-iac/_git/az-publicip?ref=main"
+
+  public_ip_names = ["bastion"]
+  subscription = "dev"
+  region_short = "we"
+  environment = "dev"
+  product = "${each.value}"
+  resource_long_name = "not-used-in-main"
+  resource_group_name     = module.resourcegroup[each.value].name
+  resource_group_location = module.resourcegroup[each.value].location
+
+}
+
+
 
 #----------------------------------------------
 #       Enterprise Point-2-Site
