09cfcb65a0aff523bef514093adf75ce8901f1fc
ParisaMousavi <parisamoosavinezhad@yahoo.com>
2022-04-02 14:19:07 +0200

    update

--- provider.tf
+++ provider.tf
@@ -7,6 +7,10 @@ terraform {
   # }
 
   backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "parisatfstateaziac"
+    container_name       = "enterprise-network"
+    key                  = "terraform.tfstate"
   }
 
 }
